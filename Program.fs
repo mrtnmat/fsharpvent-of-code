a1de0906c83cfae83ba14712ffb4a7d951fba775
mrtnmat <mrtn.matteo@proton.me>
2024-04-14 16:09:31 +0200

    Day 1 2023

---
+++ Program.fs
@@ -0,0 +1,52 @@
+﻿module Program
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let wordNumbers =
+    [| "zero"
+       "one"
+       "two"
+       "three"
+       "four"
+       "five"
+       "six"
+       "seven"
+       "eight"
+       "nine" |]
+let parseWordNumber (line: string): string =
+    match Array.tryFindIndex ((=) line) wordNumbers with
+    | Some i -> sprintf "%A" i
+    | None -> line
+    
+let parseLine (line: string) : int =
+    seq [| "(\d)"; ".*(\d).*" |]
+    |> Seq.map Regex
+    |> Seq.map (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value)
+    |> String.concat ""
+    |> int
+
+let parseLine2 (line: string) : int =
+    [| sprintf "(\d|%s).*" (String.concat "|" wordNumbers)
+       sprintf ".*(\d|%s)" (String.concat "|" wordNumbers) |]
+    |> Seq.map Regex
+    |> Seq.map (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value)
+    |> Seq.map parseWordNumber
+    |> String.concat ""
+    |> int
+
+[<EntryPoint>]
+let main argv =
+    let filePath = Path.Combine(Environment.CurrentDirectory, "input", "2023", "day1")
+    use reader = new StreamReader(filePath)
+
+    seq {
+        while not reader.EndOfStream do
+            yield reader.ReadLine()
+    }
+    |> Seq.map (fun s -> [ parseLine s; parseLine2 s ])
+    |> Seq.fold (fun acc v -> List.map2 (fun a b -> a + b) v acc) [ 0; 0 ]
+    |> printfn "result: %A"
+
+    0
---
+++ Utils.fs
@@ -0,0 +1,18 @@
+﻿module Utils
+open System.IO
+
+let readFile (filePath: string) : Option<string> =
+    try
+        use reader = new StreamReader(filePath)
+        let contents = reader.ReadToEnd()
+        Some contents
+    with _ ->
+        None
+
+let readFileLines (filePath: string) : Option<string seq> =
+    try
+        use reader = new StreamReader(filePath)
+        let lines = seq { while not reader.EndOfStream do yield reader.ReadLine() }
+        Some lines
+    with _ ->
+        None
