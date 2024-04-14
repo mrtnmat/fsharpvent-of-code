97e30b173def6aba88dc909bfa098509a5573819
mrtnmat <mrtn.matteo@proton.me>
2024-04-14 16:25:05 +0200

    Fusion optimized

--- Program.fs
+++ Program.fs
@@ -15,24 +15,25 @@ let wordNumbers =
        "seven"
        "eight"
        "nine" |]
-let parseWordNumber (line: string): string =
+
+let parseWordNumber (line: string) : string =
     match Array.tryFindIndex ((=) line) wordNumbers with
     | Some i -> sprintf "%A" i
     | None -> line
-    
+
 let parseLine (line: string) : int =
-    seq [| "(\d)"; ".*(\d).*" |]
-    |> Seq.map Regex
-    |> Seq.map (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value)
+    [| "(\d)"; ".*(\d).*" |]
+    |> Seq.map (fun s -> Regex s |> (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value))
     |> String.concat ""
     |> int
 
 let parseLine2 (line: string) : int =
     [| sprintf "(\d|%s).*" (String.concat "|" wordNumbers)
        sprintf ".*(\d|%s)" (String.concat "|" wordNumbers) |]
-    |> Seq.map Regex
-    |> Seq.map (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value)
-    |> Seq.map parseWordNumber
+    |> Seq.map (fun s ->
+        Regex s
+        |> (fun (r: Regex) -> r.Match(line).Groups.Item(1).Value)
+        |> parseWordNumber)
     |> String.concat ""
     |> int
 
