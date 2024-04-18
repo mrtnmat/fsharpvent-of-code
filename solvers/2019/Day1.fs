e697989a3170bb657611650463f628355ec5e02f
Matteo Martini <mrtn.matteo@proton.me>
2024-04-18 17:48:11 +0200

    2019 day: full solution

--- solvers/2019/Day1.fs
+++ solvers/2019/Day1.fs
@@ -15,33 +15,26 @@ module Day1 =
             }
             |> Seq.map (int)
             |> Seq.toList
-
-        let allPairsFromList (lst: 'a list) =
-            let rec f acc l =
-                match l with
-                | [] -> acc
-                | [ _ ] -> acc
-                | (x :: xs) -> f ((List.allPairs [ x ] xs) :: acc) xs
-
-            f [] lst |> List.concat
-
-        let allTriples (inp: int list) : int list seq =
-            seq {
-                for i in 1 .. List.length inp do
-                    for j in i + 1 .. List.length inp do
-                        for k in j + 1 .. List.length inp do
-                            yield [ i; j; k ]
-            }
-            |> Seq.map (List.map (fun i -> inp.[i - 1]))
+        
+        let fuelReq (mass: int) =
+            mass / 3 - 2
+        
+        let fuelReq2 (mass: int) =
+            let rec f (m: int) (acc: int) =
+                match (fuelReq m) with
+                | fuel when fuel < 1 -> acc
+                | fuel -> f fuel (acc + fuel)
+            f mass 0
 
     let solvePart1 (filePath: string) : int =
         Local.parseInput filePath
-        |> Local.allPairsFromList
-        |> List.filter(fun (a, b) -> a + b = 2020)
-        |> List.head
-        |> (fun (a, b) -> a * b)
+        |> List.map(Local.fuelReq)
+        |> List.fold(fun s v -> s + v) 0
 
-    let solvePart2 (filePath: string) : int = 0
+    let solvePart2 (filePath: string) : int =
+        Local.parseInput filePath
+        |> List.map(Local.fuelReq2)
+        |> List.fold(fun s v -> s + v) 0
 
     let solve (filePath: string) =
         (solvePart1 filePath, solvePart2 filePath)
