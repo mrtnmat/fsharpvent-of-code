168ef3555e0f87ed8fe6353db271c0ea8c4d0a0f
Matteo Martini <mrtn.matteo@proton.me>
2024-04-18 17:54:27 +0200

    project fix

---
+++ solvers/2018/Day1.fs
@@ -0,0 +1,34 @@
+namespace Solvers.Y2019
+
+open System.IO
+
+module Day1 =
+
+    module private Local =
+
+        let parseInput (path: string) =
+            use s = new StreamReader(path)
+
+            seq {
+                while not s.EndOfStream do
+                    yield s.ReadLine()
+            }
+            |> Seq.map (int)
+            |> Seq.toList
+        
+        let fuelReq (mass: int) =
+            mass / 3 - 2
+        
+        let fuelReq2 (mass: int) =
+            let rec f (m: int) (acc: int) =
+                match (fuelReq m) with
+                | fuel when fuel < 1 -> acc
+                | fuel -> f fuel (acc + fuel)
+            f mass 0
+
+    let solvePart1 (filePath: string) : int = 0
+
+    let solvePart2 (filePath: string) : int = 0
+
+    let solve (filePath: string) =
+        (solvePart1 filePath, solvePart2 filePath)
