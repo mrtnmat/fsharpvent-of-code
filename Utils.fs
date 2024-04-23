721bf1f8bff0786eaecf237f270d727bcabae8e1
mrtnmat <mrtn.matteo@proton.me>
2024-04-24 01:52:02 +0200

    format

--- Utils.fs
+++ Utils.fs
@@ -1,4 +1,5 @@
 ﻿module Utils
+
 open System.IO
 
 let readFile (filePath: string) : Option<string> =
@@ -12,7 +13,13 @@ let readFile (filePath: string) : Option<string> =
 let readFileLines (filePath: string) : Option<string seq> =
     try
         use reader = new StreamReader(filePath)
-        let lines = seq { while not reader.EndOfStream do yield reader.ReadLine() }
+
+        let lines =
+            seq {
+                while not reader.EndOfStream do
+                    yield reader.ReadLine()
+            }
+
         Some lines
     with _ ->
         None
