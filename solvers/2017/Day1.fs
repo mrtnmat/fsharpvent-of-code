164d32596f70ea8672736daae2164ce5209b49b0
mrtnmat <mrtn.matteo@proton.me>
2024-04-24 01:56:02 +0200

    2017 day1: part 2

--- solvers/2017/Day1.fs
+++ solvers/2017/Day1.fs
@@ -16,12 +16,12 @@ module Day1 =
             |> Seq.filter (fun n -> n >= 0 && n <= 9)
             |> Seq.toList
 
-        let rec loopSeq (lst: 'a seq) =
+        let rec loopList (lst: 'a list) =
             seq {
                 for item in lst do
                     yield item
 
-                yield! loopSeq lst
+                yield! loopList lst
             }
 
     let solvePart1 (filePath: string) : int =
@@ -33,11 +33,21 @@ module Day1 =
                 match v1 = v2 with
                 | true -> v1
                 | false -> 0)
-            (Local.loopSeq input |> Seq.skip 1)
+            (Local.loopList input |> Seq.skip 1)
         |> Seq.reduce (fun a v -> a + v)
 
 
-    let solvePart2 (filePath: string) : int = 0
+    let solvePart2 (filePath: string) : int =
+        let input = Local.parseInput filePath
+
+        input
+        |> Seq.map2
+            (fun v1 v2 ->
+                match v1 = v2 with
+                | true -> v1
+                | false -> 0)
+            (Local.loopList input |> Seq.skip (List.length input / 2))
+        |> Seq.reduce (fun a v -> a + v)
 
     let solve (filePath: string) =
         (solvePart1 filePath, solvePart2 filePath)
